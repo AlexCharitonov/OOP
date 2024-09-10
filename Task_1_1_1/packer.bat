25327df27073b36796cbca9b3ac0b2790f8dd830
AlexCharitonov <alexcharitonov307@mail.ru>
2024-09-10 20:52:03 +0700

    Task_1_1_1

--- Task_1_1_1/packer.bat
+++ Task_1_1_1/packer.bat
@@ -1,4 +1,5 @@
 javac -d bin ./src/main/java/org/example/Heap.java
 javac -d bin ./src/main/java/org/example/Main.java
-java ./bin/Heap ./bin/Main
 javadoc -d doc ./src/main/java/org/example/Heap.java ./src/main/java/org/example/Main.java
+cd ./bin
+java org/example/Main org/example/Heap
