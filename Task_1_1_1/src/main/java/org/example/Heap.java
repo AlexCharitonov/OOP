d0e6c4748a3036e4d6e939146f9c869805067499
AlexCharitonov <alexcharitonov307@mail.ru>
2024-09-10 11:42:46 +0700

    Task_1_1_1

--- Task_1_1_1/src/main/java/org/example/Heap.java
+++ Task_1_1_1/src/main/java/org/example/Heap.java
@@ -4,6 +4,7 @@ public class Heap {
 
     /**
      * calls function to build heap and sorts it.
+     *
      * @return array
      */
     public static int[] heapsort(int[] array) {
@@ -12,7 +13,7 @@ public class Heap {
             int help = array[i];
             array[i] = array[0];
             array[0] = help;
-            heap_sift_down(array, i, 0);
+            heapSiftDown(array, i, 0);
         }
         return array;
     }
@@ -22,33 +23,34 @@ public class Heap {
      */
     private static void buildHeap(int[] array) {
         for (int i = array.length / 2 - 1; i >= 0; i--) {
-            heap_sift_down(array, array.length, i);
+            heapSiftDown(array, array.length, i);
         }
     }
 
     /**
      * The function takes a vertex and its right and left child.
-     * Then they are compared with each other, and if the vertex is smaller than at least one of the children,
+     * Then they are compared with each other,
+     * then if the vertex is smaller than at least one of the children,
      * then vertex switch places with the biggest child,
      * then the function starts again trying to place vertex in the most bottom place that possible
      */
-    private static void heap_sift_down(int[] array, int current_length_of_array, int vertex) {
-        int id_of_biggest_value_in_family = vertex;
-        int id_of_left_child = 2 * vertex + 1;
-        int id_of_right_child = 2 * vertex + 2;
-        if (id_of_left_child < current_length_of_array &&
-                array[id_of_left_child] > array[id_of_biggest_value_in_family]) {
-            id_of_biggest_value_in_family = id_of_left_child;
+    private static void heapSiftDown(int[] array, int currentLengthOfArray, int vertex) {
+        int idOfBiggestValueInFamily = vertex;
+        int idOfLeftChild = 2 * vertex + 1;
+        int idOfRightChild = 2 * vertex + 2;
+        if (idOfLeftChild < currentLengthOfArray &&
+                array[idOfLeftChild] > array[idOfBiggestValueInFamily]) {
+            idOfBiggestValueInFamily = idOfLeftChild;
         }
-        if (id_of_right_child < current_length_of_array &&
-                array[id_of_right_child] > array[id_of_biggest_value_in_family]) {
-            id_of_biggest_value_in_family = id_of_right_child;
+        if (idOfRightChild < currentLengthOfArray &&
+                array[idOfRightChild] > array[idOfBiggestValueInFamily]) {
+            idOfBiggestValueInFamily = idOfRightChild;
         }
-        if (id_of_biggest_value_in_family != vertex) {
+        if (idOfBiggestValueInFamily != vertex) {
             int help = array[vertex];
-            array[vertex] = array[id_of_biggest_value_in_family];
-            array[id_of_biggest_value_in_family] = help;
-            heap_sift_down(array, current_length_of_array, id_of_biggest_value_in_family);
+            array[vertex] = array[idOfBiggestValueInFamily];
+            array[idOfBiggestValueInFamily] = help;
+            heapSiftDown(array, currentLengthOfArray, idOfBiggestValueInFamily);
         }
     }
 }
