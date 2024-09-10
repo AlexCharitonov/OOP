905cc20c2c36e91375f43479df44d4f9c29c24cb
AlexCharitonov <alexcharitonov307@mail.ru>
2024-09-10 18:43:48 +0700

    Task_1_1_1

--- Task_1_1_1/src/test/java/org/example/Test3.java
+++ Task_1_1_1/src/test/java/org/example/Test3.java
@@ -14,8 +14,7 @@ public class Test3 {
 
     @Test
     void main() {
-        int[] arr = {-100, -1324, -600, -300, 0, 100, 1324, 600, 300, 0, 700, -700};
-        Heap.heapsort(arr);
+        int[] arr = Heap.heapsort(new int[] {-100, -1324, -600, -300, 0, 100, 1324, 600, 300, 0, 700, -700});
         int[] trueArr = {-1324, -700, -600, -300, -100, 0, 0, 100, 300, 600, 700, 1324};
         for (int i = 0; i < trueArr.length; i++) {
             assertEquals(trueArr[i], arr[i]);
