88108f408ee0d752be6d6adf0629620d70295c94
AlexCharitonov <alexcharitonov307@mail.ru>
2024-10-22 08:04:52 +0700

    Task_1_1_1

--- Task_1_1_1/packer.bat
+++ Task_1_1_1/packer.bat
@@ -1,5 +1,6 @@
-javac -d bin ./src/main/java/org/example/Heap.java
-javac -d bin ./src/main/java/org/example/Main.java
-javadoc -d doc ./src/main/java/org/example/Heap.java ./src/main/java/org/example/Main.java
+javac -d bin ./src/main/java/org/example/Heap.java ./src/main/java/org/example/Main.java
+"C:\Program Files\Java\jdk-22\bin\javadoc.exe" -d doc ./src/main/java/org/example/Heap.java ./src/main/java/org/example/Main.java
 cd ./bin
+"C:\Program Files\Java\jdk-22\bin\jar.exe" cvf /org/example/heap.jar /org/example/Main.class /org/example/Heap.class
+"C:\Program Files\Java\jdk-22\bin\jar.exe" --create --file heap.jar --main-class=org/example/Main Main.class Heap.class
 java org/example/Main org/example/Heap
