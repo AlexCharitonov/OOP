9bed8a8001162b4d799b26ba6c0d8fc73986d75f
AlexCharitonov <alexcharitonov307@mail.ru>
2024-11-25 22:37:39 +0700

    Task_1_1_3

--- Task_1_1_3/src/main/java/org/example/Main.java
+++ Task_1_1_3/src/main/java/org/example/Main.java
@@ -19,14 +19,8 @@ public class Main {
         Expression expr = Parser.parse(str);
         expr.print();
         System.out.print("\n");
-        Expression derive = expr.derivative("x");
-        derive.print();
-        System.out.print("\n");
         Expression simply = expr.simplification();
         simply.print();
-        System.out.print("\n");
-        Expression simplyder = derive.simplification();
-        simplyder.print();
         in.close();
     }
 }
