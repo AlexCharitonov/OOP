c615c508e98246629b1d8f5cb6fe545d5fddbbfd
AlexCharitonov <alexcharitonov307@mail.ru>
2024-12-10 01:00:16 +0700

    Task_1_5_1

--- Task_1_5_1/src/test/java/org/example/markdown/MarkDownTest.java
+++ Task_1_5_1/src/test/java/org/example/markdown/MarkDownTest.java
@@ -57,12 +57,10 @@ public class MarkDownTest {
 
     @Test
     void linkTest() {
-        Image.ImageBuilder builder = new Image.ImageBuilder();
-        builder.setText("image");
-        builder.setLink("https://images.unsplash.com/photo-1571992049393-827d13da8fe3?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
-        Image.ImageBuilder builder1 = new Image.ImageBuilder();
-        builder1.setText(new Text.TextBuilder().setText("image").build());
-        builder1.setLink("https://images.unsplash.com/photo-1571992049393-827d13da8fe3?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+        Link.LinkBuilder builder = new Link.LinkBuilder();
+        builder.setLink("https://mapgenie.io/god-of-war-2018/maps/midgard");
+        Link.LinkBuilder builder1 = new Link.LinkBuilder();
+        builder1.setLink(new Text.TextBuilder().setText("https://mapgenie.io/god-of-war-2018/maps/midgard").build());
         assertTrue(builder.build().equals(builder1.build()));
     }
 
