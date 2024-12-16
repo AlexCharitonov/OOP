b1a829385b2c1f31b056de18a83ac95a2ab6d780
AlexCharitonov <alexcharitonov307@mail.ru>
2024-12-16 21:00:01 +0700

    Task_1_5_1

--- Task_1_5_1/src/main/java/org/example/Main.java
+++ Task_1_5_1/src/main/java/org/example/Main.java
@@ -1,24 +1,16 @@
 package org.example;
 
+import org.example.markdown.CodeBlocks;
 import org.example.markdown.Table;
+import org.example.markdown.Text;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Table.TableBuilder builder = new Table.TableBuilder();
-        builder.withAlignments(Table.Align.ALIGN_RIGHT,
-                        Table.Align.ALIGN_CENTER, Table.Align.ALIGN_LEFT)
-                .setMaxRows(5)
-                .withRowLimit(15)
-                .addRow("Index", "Index x2", "Index * Index");
-        for (int i = 0; i < 3; i++) {
-            builder.addRow(
-                    Integer.toString(i),
-                    Integer.toString(i * 2),
-                    Integer.toString(i * i)
-            );
-        }
+        CodeBlocks.CodeBlocksBuilder builder = new CodeBlocks.CodeBlocksBuilder();
+        builder = builder.addString(new Text.TextBuilder()
+                .setText("print(\"Test_1_5_1\");").build());
         System.out.println(builder.build());
     }
 }
